ae34bfbd74a3030a683f4b0bbc9717533ac2616d
wechanhe <1470325185@qq.com>
2019-09-09 11:12:52 +0800

    add test

---
+++ src/part1/Test.java
@@ -0,0 +1,12 @@
+package part1;
+
+/**
+ * question:
+ * solution:
+ * discussion:
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hh");
+    }
+}
